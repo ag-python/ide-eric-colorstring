32e7fb6519d904a79d4ff03be1808138a5245142
Jaime Seuma <jaims.seuma@gmail.com>
2014-03-24 17:18:28 +0100

    eric5 Color string plugin i18n-es update default branch

--- ColorString/i18n/colorstring_es.ts
+++ ColorString/i18n/colorstring_es.ts
@@ -1,22 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="es_ES">
+<TS version="2.0" language="es_ES">
 <context>
     <name>ColorSelectionDialog</name>
     <message>
         <location filename="../../ColorString/ColorSelectionDialog.ui" line="14"/>
         <source>Color String</source>
-        <translation type="unfinished"></translation>
+        <translation>Cadena de Color</translation>
     </message>
     <message>
         <location filename="../../ColorString/ColorSelectionDialog.ui" line="23"/>
         <source>Select Color Name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Seleccionar Nombre de Color:</translation>
     </message>
     <message>
         <location filename="../../ColorString/ColorSelectionDialog.ui" line="30"/>
         <source>Select a color by name</source>
-        <translation type="unfinished"></translation>
+        <translation>Seleccionar un color por nombre</translation>
     </message>
 </context>
 <context>
@@ -24,17 +24,17 @@
     <message>
         <location filename="../../PluginColorString.py" line="267"/>
         <source>Color String</source>
-        <translation type="unfinished"></translation>
+        <translation>Cadena de Color</translation>
     </message>
     <message>
         <location filename="../../PluginColorString.py" line="218"/>
         <source>&lt;p&gt;The selected string &lt;b&gt;{0}&lt;/b&gt; is not a valid color string. Aborting!&lt;/p&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;p&gt;La cadena seleccionada&lt;b&gt;{0}&lt;/b&gt; no es una cadena de color válida. ¡Abortando!&lt;/p&gt;</translation>
     </message>
     <message>
         <location filename="../../PluginColorString.py" line="267"/>
         <source>&lt;p&gt;The selected string &lt;b&gt;{0}&lt;/b&gt; is not a valid color name. Aborting!&lt;/p&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;p&gt;La cadena seleccionada&lt;b&gt;{0}&lt;/b&gt; no es un nombre de color válido. ¡Abortando!&lt;/p&gt;</translation>
     </message>
 </context>
 </TS>
