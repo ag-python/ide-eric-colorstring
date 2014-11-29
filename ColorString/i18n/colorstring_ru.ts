360c78c0a5c014f965f72b7546609acabbd7a6ee
Detlev Offenbach <detlev@die-offenbachs.de>
2014-11-29 11:44:14 +0100

    Added Russian translations provided by Mikhail Terekhov.

---
+++ ColorString/i18n/colorstring_ru.ts
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0" language="ru" sourcelanguage="en">
+<context>
+    <name>ColorSelectionDialog</name>
+    <message>
+        <location filename="../../ColorString/ColorSelectionDialog.ui" line="14"/>
+        <source>Color String</source>
+        <translation>Цвет</translation>
+    </message>
+    <message>
+        <location filename="../../ColorString/ColorSelectionDialog.ui" line="23"/>
+        <source>Select Color Name:</source>
+        <translation>Название цвета:</translation>
+    </message>
+    <message>
+        <location filename="../../ColorString/ColorSelectionDialog.ui" line="30"/>
+        <source>Select a color by name</source>
+        <translation>Выберите цвет по названию</translation>
+    </message>
+</context>
+<context>
+    <name>ColorStringPlugin</name>
+    <message>
+        <location filename="../../PluginColorString.py" line="269"/>
+        <source>Color String</source>
+        <translation>Цвет</translation>
+    </message>
+    <message>
+        <location filename="../../PluginColorString.py" line="220"/>
+        <source>&lt;p&gt;The selected string &lt;b&gt;{0}&lt;/b&gt; is not a valid color string. Aborting!&lt;/p&gt;</source>
+        <translation>&lt;p&gt;Выбранная строка &lt;b&gt;{0}&lt;/b&gt; не является правильной для определения цвета. Авост!&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <location filename="../../PluginColorString.py" line="269"/>
+        <source>&lt;p&gt;The selected string &lt;b&gt;{0}&lt;/b&gt; is not a valid color name. Aborting!&lt;/p&gt;</source>
+        <translation>&lt;p&gt;Выбранная строка &lt;b&gt;{0}&lt;/b&gt; не является правильным разванием цвета. Авост!&lt;/p&gt;</translation>
+    </message>
+</context>
+</TS>
